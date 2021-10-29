9f8cf458e9b215037c3f3c16ff4516cc691826d3
Heder-Silva-Oliveira <hedersilva2@gmail.com>
2021-10-29 20:48:57 -0300

    002

--- Analise.py
+++ Analise.py
@@ -2,5 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 plt.style.use("seaborn")
 
-df = pd.read_excel("AdventureWorks.xlsx")
-print(df.head())
+
+
+df = pd.read_excel("AdventureWorks.xlsx" )
+df["Valor Venda"].sum()
+df["custo"] = df["Custo Unitário"].mul(df["Quantidade"])
+df["lucro"] = df["Valor Venda"] - df["custo"]
+
+df.groupby("Produto")["Quantidade"].sum().sort_values(ascending=True).plot.barh(title="Total Produtos Vendidos")
+print(plt.xlabel("Total"))
+print(plt.ylabel("produto"))
+
+df.groupby(df["Data Venda"].dt.year)["lucro"].sum().plot.bar(title="Lucro anual")
+plt.xlabel("Ano")
+plt.ylabel("Receita");
